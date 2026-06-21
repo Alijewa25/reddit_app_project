4b13f3872941ce08bcb74c417f32e0165e9c6bd3
bayramovh062-ui <bayramovh062@gmail.com>
2026-06-21 16:19:24 +0400

    feat: implement database upsert logic and DB session plumbing

--- data-pipeline/pipeline/db.py
+++ data-pipeline/pipeline/db.py
@@ -38,8 +38,7 @@ def init_db():
     TODO:
         - Call Base.metadata.create_all(bind=engine)
     """
-    pass  # Remove this line when you implement the function
-
+    Base.metadata.create_all(bind=engine)
 
 def get_session():
     """
@@ -53,4 +52,4 @@ def get_session():
     TODO:
         - Return SessionLocal()
     """
-    pass  # Remove this line when you implement the function
+    return SessionLocal()
